78b621f50b05d09c0c4d837c4e680653c5904c3b
Jan Bajena <bajena3@gmail.com>
2016-10-01 15:18:12 +0200

    Add user registration mutation
    closes https://github.com/Bajena/wod-journal-api/issues/15

--- app/graph/types/mutation_type.rb
+++ app/graph/types/mutation_type.rb
@@ -2,4 +2,5 @@ MutationType = GraphQL::ObjectType.define do
   name "Mutation"
 
   field :CreateWod, field: WodMutations::CreateWod.field
+  field :RegisterUser, field: UserMutations::RegisterUser.field
 end
